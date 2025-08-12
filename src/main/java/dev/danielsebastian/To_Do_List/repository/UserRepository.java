c6a4bd3fab55c058c12bc44a231f4d14fb5eeece
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-08-12 14:25:10 -0300

    feat(repository): add UserRepository

---
+++ src/main/java/dev/danielsebastian/To_Do_List/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package dev.danielsebastian.To_Do_List.repository;
+
+import dev.danielsebastian.To_Do_List.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<UserDetails> findUserByEmail(String email);
+}
