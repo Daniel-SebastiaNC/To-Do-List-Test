5f661c35d5f3c3bf5c1a057715072287b2e4032d
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-08-13 15:02:39 -0300

    feat(Repository): add findAllByOrderByPriorityDesc Pageable on TaskRepository

--- src/main/java/dev/danielsebastian/To_Do_List/repository/TaskRepository.java
+++ src/main/java/dev/danielsebastian/To_Do_List/repository/TaskRepository.java
@@ -1,6 +1,8 @@
 package dev.danielsebastian.To_Do_List.repository;
 
 import dev.danielsebastian.To_Do_List.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
+    Page<Task> findAllByOrderByPriorityDesc(Pageable pageable);
 }
