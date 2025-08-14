8a93a56340714e49c030c53bc1a68eac20b439f0
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-08-13 21:33:14 -0300

    feat: add endpoint GET to getAllTaskBoards

--- src/main/java/dev/danielsebastian/To_Do_List/controller/TaskBoardController.java
+++ src/main/java/dev/danielsebastian/To_Do_List/controller/TaskBoardController.java
@@ -4,12 +4,12 @@ import dev.danielsebastian.To_Do_List.dto.task_board.TaskBoardRequest;
 import dev.danielsebastian.To_Do_List.dto.task_board.TaskBoardResponse;
 import dev.danielsebastian.To_Do_List.service.TaskBoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/task-board")
@@ -21,5 +21,10 @@ public class TaskBoardController {
     @PostMapping("/create")
     public ResponseEntity<TaskBoardResponse> createTaskBoard(@RequestBody TaskBoardRequest taskBoardRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(taskBoardService.createTaskBoard(taskBoardRequest));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<TaskBoardResponse>> getAllTaskBoards(Pageable pageable) {
+        return ResponseEntity.ok(taskBoardService.getAllTaskBoards(pageable));
     }
 }
--- src/main/java/dev/danielsebastian/To_Do_List/repository/TaskBoardRepository.java
+++ src/main/java/dev/danielsebastian/To_Do_List/repository/TaskBoardRepository.java
@@ -1,6 +1,8 @@
 package dev.danielsebastian.To_Do_List.repository;
 
 import dev.danielsebastian.To_Do_List.model.TaskBoard;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface TaskBoardRepository extends JpaRepository<TaskBoard, UUID> {
+    Page<TaskBoard> findAll(Pageable pageable);
 }
